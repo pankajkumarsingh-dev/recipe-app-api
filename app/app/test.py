0668fe290644fda8f4670e94ca6058ae35443446
pankaj-2503 <pankajpj3103@gmail.com>
2025-11-30 14:15:55 +0530

    add project

--- app/app/test.py
+++ app/app/test.py
@@ -2,18 +2,21 @@
 Sample tests
 """
 from django.test import SimpleTestCase
+
 from app import calc
 
 
-class CalcTest(SimpleTestCase):
-   """Test the calc module"""
-   # Here test class should start with test unless it would not get picked up by test class
-   def test_add_numbers(self):
-      """Test adding numbers together."""
-      res=calc.add(2,8)
-      self.assertEqual(res,10)
-    
-   def test_subtract_numbers(self):
-      """Test subracting numbers together"""
-      res=calc.subtract(5,2)
-      self.assertEqual(res,3)
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
